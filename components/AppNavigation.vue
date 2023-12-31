598d1d1f5453d4c49a3518956a4ae976542428b1
Ewanretor Okugbe <ewanretorokugbe@gmail.com>
2023-12-31 16:48:13 +0100

    Icons Update

---
+++ components/AppNavigation.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex md:h-screen max-md:justify-center md:items-center max-md:bottom-0 md:left-0">
+    <div
+      class="w-[97%] my-[1vh] h-[7vh] md:w-20 md:h-[97vh] border rounded-3xl border-[var(--border-color)] border-opacity-60"
+    ></div>
+  </div>
+</template>
--- pages/lobby.vue
+++ pages/lobby.vue
@@ -1,3 +1,24 @@
-<template></template>
+<template>
+  <Head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,700;1,400&display=swap"
+      rel="stylesheet"
+    />
+  </Head>
+  <title>BottleHub - Gambling on The Edge</title>
+  <div class="md:flex md:justify-center md:min-h-screen">
+    <div class="grid grid-cols-1 md:grid-cols-2 md:h-screen">
+      <div
+        class="flex h-[89vh] md:h-[97vh] w- max-md:justify-center md:items-center max-md:bottom-0 md:left-0 my-[1vh]"
+      >
+      </div>
+      <AppNavigation />
+    </div>
+  </div>
+</template>
 <script setup lang="ts"></script>
 <style></style>
