6de30bbd54d09275f789b0dc5536145ba3243c78
Ewanretor Okugbe <ewanretorokugbe@gmail.com>
2024-01-18 19:01:24 +0100

    Styling Navigation

--- components/CustomLink.ts
+++ components/CustomLink.ts
@@ -1,5 +1,5 @@
 export default defineNuxtLink({
   componentName: "CustomLink",
-  activeClass: "font-bold underline",
+  activeClass: "font-bold underline active-link",
   exactActiveClass: "font-medium underline",
 });
