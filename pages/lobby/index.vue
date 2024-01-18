a33d483b923fd0c8bed8652b9985eac57a736d00
Ewanretor Okugbe <ewanretorokugbe@gmail.com>
2024-01-18 16:12:28 +0100

    Fixes

--- pages/lobby/index.vue
+++ pages/lobby/index.vue
@@ -2,12 +2,24 @@
   <div class="w-full min-h-[67vh] flex items-center justify-center">
     <Head> </Head>
     <title>BottleHub - Gambling on The Edge</title>
-    <div class="hero-content max-w-full w-full"></div>
+    <div class="hero-content max-w-full w-full">
+      <div class="w-full justify-center text-center">
+        <h2 class="font-bold text-2xl capitalize mb-5">
+          Currently Work in Progress...
+        </h2>
+        <p>
+          In the mean time you can check out the
+          <nuxt-link href="/faucet" class="link">faucet</nuxt-link> to get test
+          tokens as well as the
+          <nuxt-link href="/documentation" class="link">docs</nuxt-link> to
+          learn more about the project.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="js" setup>
-wip.showModal();
 definePageMeta({
   pageTransition: {
     name: "slide-up",
